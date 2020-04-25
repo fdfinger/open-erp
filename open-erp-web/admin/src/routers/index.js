8ab725eeb14d47ccbd121b1a50d5d70c10408bb0
fdfinger <jiangfei.ng@foxmail.com>
2020-04-25 23:30:45 +0800

    先存一下

--- open-erp-web/admin/src/routers/index.js
+++ open-erp-web/admin/src/routers/index.js
@@ -1,76 +1,70 @@
 import React from "react";
-import Login from '../pages/Login';
-import NotFound from '../pages/NotFound';
-import Dashboard from '../pages/Dashboard';
-import Department from '../pages/Department';
-import Users from '../pages/Users';
-import Area from '../pages/Area';
-import Warehouse from '../pages/Warehouse';
-import ProcessCata from '../pages/ProcessCata';
-import Process from '../pages/Process';
-import Setting from '../pages/Setting';
 
 export const Parent = ({ children }) => {
-  return <div>{children}</div>;
-};
+  return (
+  <div>{children}</div>
+  )
+}
+
 
 export const commonRoutes = [
   {
     path: "/login",
-    component: Login,
+    component: React.lazy(() => import("../pages/Login")),
   },
   {
     path: "/404",
-    component: NotFound,
+    component: React.lazy(() => import("../pages/NotFound")),
   },
 ];
 
 export const routers = [
   {
-    path: "/dashboard",
-    title: "数据仪表",
-    component: Dashboard,
+    path: '/dashboard',
+    title: '数据仪表',
+    component: React.lazy(() => import("../pages/Dashboard"))
   },
   {
-    path: "/system",
-    title: "系统管理",
+    path: '/system',
+    title: '系统管理',
     component: Parent,
     childrens: [
+
       {
-        path: "/system/department",
-        title: "部门管理",
-        component: Department,
+        path: '/system/department',
+        title: '部门管理',
+        component: React.lazy(() => import("../pages/Department"))
       },
       {
-        path: "/system/users",
-        title: "用户信息",
-        component: Users,
+        path: '/system/users',
+        title: '用户信息',
+        component: React.lazy(() => import("../pages/Users"))
       },
       {
-        path: "/system/area",
-        title: "地区管理",
-        component: Area,
+        path: '/system/area',
+        title: '地区管理',
+        component: React.lazy(() => import("../pages/Area"))
       },
       {
-        path: "/system/warehouse",
-        title: "仓库管理",
-        component: Warehouse,
+        path: '/system/warehouse',
+        title: '仓库管理',
+        component: React.lazy(() => import("../pages/Warehouse"))
       },
       {
-        path: "/system/processCata",
-        title: "工序分类",
-        component: ProcessCata,
+        path: '/system/processCata',
+        title: '工序分类',
+        component: React.lazy(() => import("../pages/ProcessCata"))
       },
       {
-        path: "/system/process",
-        title: "工序管理",
-        component: Process,
+        path: '/system/process',
+        title: '工序管理',
+        component: React.lazy(() => import("../pages/Process"))
       },
       {
-        path: "/system/setting",
-        title: "个人设置",
-        component: Setting,
+        path: '/system/setting',
+        title: '个人设置',
+        component: React.lazy(() => import("../pages/Setting"))
       },
-    ],
+    ]
   },
 ];
--- open-erp-web/admin/src/store/sagas/loginSaga.js
+++ open-erp-web/admin/src/store/sagas/loginSaga.js
@@ -1,19 +1,19 @@
-import { takeEvery, select, call, put } from "redux-saga/effects";
+import { throttle,  select, call, put } from "redux-saga/effects";
 import { push } from "react-router-redux";
 import api from "../../api";
 import { LOGIN_SUBMIT, LOGIN_SUCCESS, LOGIN_ERROR } from "../constant/login";
 
 export function* loginSaga() {
-  yield takeEvery(LOGIN_SUBMIT, function* () {
+  yield throttle(3000, LOGIN_SUBMIT, function* () {
     try {
       const state = yield select((state) => state.login);
       const res = yield call(api.login.login, state);
-      if ('data' in res) {
+      if (res.data) {
         yield put({
           type: LOGIN_SUCCESS,
           value: res,
         });
-        yield put(push("/admin/dashboard"));
+        yield put(push("/dashboard"));
       } else {
         yield put({
           type: LOGIN_ERROR,
