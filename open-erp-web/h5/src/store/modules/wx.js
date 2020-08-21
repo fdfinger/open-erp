8aa063ad11d5832c4d2a733ffabeb22c5cf873ed
Jiangfei <jiangfei.ng@foxmail.com>
2020-08-21 16:44:26 +0800

    调整

--- open-erp-web/h5/src/store/modules/wx.js
+++ open-erp-web/h5/src/store/modules/wx.js
@@ -9,7 +9,7 @@ const mutations = {}
 
 const actions = {
   async ready({state}){
-    const data = await wxApi.getjssdk({ url: encodeURIComponent(window.location.href.split("#")[0]) })
+    const data = await wxApi.getjssdk({ url: window.location.href.split("#")[0] })
     wx.config({
       ...data,
       debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
